75443569a6b043bdda49bda8635357684bcd0a7e
unknown <jsiu125@gmail.com>
2019-04-10 13:34:34 -0700

    Lab finished

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -4,26 +4,28 @@ from lab1 import *
 class TestLab1(unittest.TestCase):
 
     def test_max_list_iter(self):
-        """add description here"""
         self.assertEqual(max_list_iter([]), None)  # checks for an empty list passing into function, returns None
-        self.assertEqual(max_list_iter([2,5,6,8]),8)  # returns max value in a given list
+        self.assertEqual(max_list_iter([2, 5, 6, 8]), 8)  # returns max value in a given list
+        self.assertEqual(max_list_iter([3, 6, 1, 6, 8, 3, 85, 85]), 85)
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
 
     def test_reverse_rec(self):
-        self.assertEqual(reverse_rec([1,2,3]),[3,2,1])  # returns reversal of a given list
-        self.assertEqual(reverse_rec([1]),[1])
+        self.assertEqual(reverse_rec([1, 2, 3]), [3, 2, 1])  # returns reversal of a given list
+        self.assertEqual(reverse_rec([1]), [1])
         self.assertEqual(reverse_rec([]), [])  # checks for empty list passing into function, returns empty list
         intlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             reverse_rec(intlist)
 
     def test_bin_search(self):
-        list_val =[0,1,2,3,4,7,8,9,10]
+        list_val =[0, 1, 2, 3, 4, 7, 8, 9, 10]
         low = 0
         high = len(list_val)-1
         self.assertEqual(bin_search(4, low, high, list_val), 4)  # returns index of target value
+        self.assertEqual(bin_search(1, low, high, list_val), 1)
+        self.assertEqual(bin_search(8, low, high, list_val), 6)
         self.assertEqual(bin_search(6, low, high, list_val), None)  # checks if a target value exists in a given list, returns None
         list2_val = [13]
         high2 = len(list2_val)-1
--- location_tests.py
+++ location_tests.py
@@ -6,11 +6,19 @@ class TestLab1(unittest.TestCase):
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
-    # Add more tests!
+
     def test_eq(self):
-        lat = Location()
-        self.assertEqual(eq(loc), )
+        loc = Location("SLO", 35.3, -120.7)
+        loc2 = Location("Paris", 48.9, 2.4)
+        loc4 = Location("SLO", 35.3, -120.7)
+        self.assertTrue(loc == loc4)
+        self.assertFalse(loc == loc2)
+
+    def test_init(self):
+        loc = Location("SF", 37.8, -122.4)
+        self.assertEqual(loc.name, "SF")
+        self.assertEqual(loc.lat, 37.8)
+        self.assertEqual(loc.lon, -122.4)
 
 if __name__ == "__main__":
         unittest.main()
